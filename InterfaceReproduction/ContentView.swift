662e33a5954f999dcc3a9e00162ce7fc579f4dd2
Kiho Okazawa <kokazawa@lcs.on.ca>
2022-11-17 09:21:06 -0500

    Made a background

--- InterfaceReproduction/ContentView.swift
+++ InterfaceReproduction/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            
+            //First layer (background)
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack{
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
