5dddd694126da9eacaeb8e8376d1b5ae11406d25
Kiho Okazawa <kokazawa@lcs.on.ca>
2022-11-18 12:07:46 -0500

    Got two examples in two different texts

--- InterfaceReproduction/ContentView.swift
+++ InterfaceReproduction/ContentView.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 struct ContentView: View {
+    
+    var firstExample: AttributedString {
+        
+        let welcomeString = "Welcome"
+        var welcomeAttributedString = AttributedString(welcomeString)
+        welcomeAttributedString.font = .body.bold()
+        welcomeAttributedString.foregroundColor = .green
+        
+        
+        
+        
+        
+        return welcomeAttributedString
+        
+    }
+    
+    var secondExample: AttributedString {
+        var house = AttributedString(" to our new house\n")
+        house.font = .body
+        house.foregroundColor = .red
+         return house
+        
+    }
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -23,42 +47,68 @@ struct ContentView: View {
                         Text("No Workouts")
                             .foregroundColor(.white)
                             .font(Font.system(size: 20, weight: .heavy))
-                            
-                    
-                        Text("Please check whether you have granted permissions to read Health data in")
-                        + Text(" Settings").foregroundColor(.green).fontWeight(.bold)
-        
-                        + Text(" 　 →")
-                        + Text(" Health").foregroundColor(.green).fontWeight(.bold)
-                        + Text(" →")
-                        + Text(" Data Access & Devices").foregroundColor(.green).fontWeight(.bold)
-                        + Text(" →")
-                        + Text("   Workout Shoes").foregroundColor(.green).fontWeight(.bold)
                         
                         
-                        
-                        
-                        
-                        
-                        
+                        Text("Please check whether you have granted permissions to read Health data in " + firstExample + secondExample)
                     }
                     .padding()
                 }
                 .navigationTitle("Workouts")
             }
         }
-        
-        
     }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-                .preferredColorScheme(.dark)
-        }
-    }
-    
-    
-    
-    
 }
+
+
+
+
+
+
+
+//                        + Text(" Settings").foregroundColor("Yellowgreen").fontWeight(.bold)
+//
+//                        + Text(" 　 →")
+//                        + Text(" Health").foregroundColor(.green).fontWeight(.bold)
+//                        + Text(" →")
+//                        + Text(" Data Access & Devices").foregroundColor(.green).fontWeight(.bold)
+//                        + Text(" →")
+//                        + Text("   Workout Shoes").foregroundColor(.green).fontWeight(.bold)
+//
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabView {
+            ContentView()
+                .tabItem {
+                    Image(systemName: "figure.mixed.cardio")
+                    Text("Workouts")
+                }
+            Text("Shoes")
+                .tabItem {
+                    Image(systemName: "shoeprints.fill")
+                    Text("Shoes")
+                }
+            Text("Setting")
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
+        }
+        .accentColor(.green)
+        .preferredColorScheme(.dark)
+    }
+}
+
+
+
+
+
