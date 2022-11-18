6e3d0245776f7605ddd6517c1c7944bd58704827
Kiho Okazawa <kokazawa@lcs.on.ca>
2022-11-17 22:45:44 -0500

    Adding a new colour

--- InterfaceReproduction/ContentView.swift
+++ InterfaceReproduction/ContentView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
         NavigationView {
@@ -14,24 +13,52 @@ struct ContentView: View {
                 //First layer (background)
                 Color.black
                     .ignoresSafeArea()
+                Spacer()
                 
                 //Second layer
-                VStack{
+                ZStack(alignment: .topLeading) {
+                    Rectangle().foregroundColor(.black)
+                        .frame(maxWidth: .infinity, maxHeight: 550)
+                    VStack(alignment: .leading, spacing: 15){
+                        Text("No Workouts")
+                            .foregroundColor(.white)
+                            .font(Font.system(size: 20, weight: .heavy))
+                            
                     
-                    
+                        Text("Please check whether you have granted permissions to read Health data in")
+                        + Text(" Settings").foregroundColor(.green).fontWeight(.bold)
+        
+                        + Text(" 　 →")
+                        + Text(" Health").foregroundColor(.green).fontWeight(.bold)
+                        + Text(" →")
+                        + Text(" Data Access & Devices").foregroundColor(.green).fontWeight(.bold)
+                        + Text(" →")
+                        + Text("   Workout Shoes").foregroundColor(.green).fontWeight(.bold)
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
+                    .padding()
                 }
-                .padding()
+                .navigationTitle("Workouts")
             }
-            .navigationTitle("Workouts")    
+        }
+        
+        
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+                .preferredColorScheme(.dark)
         }
     }
     
     
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-            .preferredColorScheme(.dark)
-    }
+    
+    
 }
